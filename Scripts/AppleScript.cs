771f3187233074993412f9bbf773fa9d6e982c9f
Nadir Niodil <165339957+nniodil@users.noreply.github.com>
2024-08-19 18:19:13 +0200

    Update AppleScript.cs

--- Scripts/AppleScript.cs
+++ Scripts/AppleScript.cs
@@ -5,16 +5,9 @@ using UnityEngine;
 public class AppleScript : MonoBehaviour
 {
     public float rotationSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
         transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
-        
     }
 }
