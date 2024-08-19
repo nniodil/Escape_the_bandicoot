2d211e6c5f003634d43c47b3b76b88726b746450
Nadir Niodil <165339957+nniodil@users.noreply.github.com>
2024-08-19 18:20:11 +0200

    Update PizzaScript.cs

--- Scripts/PizzaScript.cs
+++ Scripts/PizzaScript.cs
@@ -5,12 +5,7 @@ using UnityEngine;
 public class PizzaScript : MonoBehaviour
 {
     public float rotationSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
