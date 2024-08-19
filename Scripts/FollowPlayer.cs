a9941e65903016b07a1c9df22a791604bb410be6
Nadir Niodil <165339957+nniodil@users.noreply.github.com>
2024-08-19 18:19:27 +0200

    Update FollowPlayer.cs

--- Scripts/FollowPlayer.cs
+++ Scripts/FollowPlayer.cs
@@ -6,17 +6,9 @@ public class FollowPlayer : MonoBehaviour
 {
     private Vector3 offset = new Vector3(0.64f, 2.06f, -2.56f);
     public GameObject player;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         transform.position = player.transform.position + offset;
     }
-
-
 }
